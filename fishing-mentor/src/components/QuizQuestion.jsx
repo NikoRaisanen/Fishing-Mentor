2e61096d706e2b60b74be7da06c8cfa4466d9d7a
unknown <nikoraisanenbusiness@gmail.com>
2022-05-19 15:42:31 -0700

    removed 2 components

--- fishing-mentor/src/components/QuizQuestion.jsx
+++
@@ -1,7 +0,0 @@
-const QuizQuestion = (props) => {
-    return (
-        <h1 style={{ 'text-align': 'center', 'marginBottom': '100px', 'marginTop': '150px' }}>{props.question}</h1>
-    )
-}
-
-export default QuizQuestion;
