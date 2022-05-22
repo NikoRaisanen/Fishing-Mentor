0340b11bf5aba0dccf6321c8596ab7562e5975a9
unknown <nraisane@asu.edu>
2022-05-22 13:38:44 -0700

    cleanup with helper.js file

--- fishing-mentor/src/App.js
+++ fishing-mentor/src/App.js
@@ -5,21 +5,9 @@ import './App.css';
 import { CardGroup } from 'react-bootstrap';
 import QuizCard from './components/QuizCard'
 import Results from './components/Results'
-import updateResults from './helpers.js'
+import { updateResults, questions, lures }  from './helpers.js'
 
 function App() {
-  const lures = {
-    'Jig': 0,
-    'Crankbait': 0,
-    'Jerkbait': 0,
-    'Swimbait': 0,
-    'Spinnerbait': 0,
-    'Wacky rig': 0,
-    'Drop shot': 0,
-    'Glidebait': 0,
-    'Topwater': 0,
-    'Chatterbait': 0
-  }
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [quizCompleted, setQuizCompleted] = useState(false)
   const [lureStatus, setLureStatus] = useState(lures)
@@ -30,68 +18,12 @@ function App() {
       setQuizCompleted(true)
     }
     // Update lureStatus with appropriate values
-    console.log(selectedCard.lurePoints)
-    console.log('Current status:' + JSON.stringify(lureStatus))
     const r = updateResults(selectedCard.lurePoints, lureStatus)
     setLureStatus(r)
-    // Add lurePoints value to lureStatus state
-    // Something like lurePoints.map() to create intermediary var, then save that var with setLureStatus
     // Increment to move on to next question
     const nextQuestion = currentQuestion + 1
     setCurrentQuestion(nextQuestion)
   }
-
-  const questions = [
-    {
-      text: 'Are you fishing from shore?',
-      options: [
-        {answer: 'Yes', lurePoints: {'Jig': 0.1, 'Crankbait': 0.1, 'Jerkbait': 0.15, 'Swimbait': 0, 'Spinnerbait': 0.05, 'Wacky rig': 0.1, 'Drop shot': -0.4, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0.05}},
-        {answer: 'No', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': 0.4, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0.05}}
-        
-      ]
-    },
-    {
-      text: 'Are you getting caught up in vegetation / debris?',
-      options: [
-        {answer: 'Yes', lurePoints: {'Jig': 0.2, 'Crankbait': -0.3, 'Jerkbait': 0, 'Swimbait': 0.1, 'Spinnerbait': -0.1, 'Wacky rig': 0.3, 'Drop shot': 0, 'Glidebait': -0.4, 'Topwater': 0.2, 'Chatterbait': 0.1}},
-        {answer: 'No', lurePoints: {'Jig': 0, 'Crankbait': 0.1, 'Jerkbait': 0.1, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0.0}}
-      ]
-    },
-    {
-      text: 'How is the bottom of the fishery?',
-      options: [
-        {answer: 'Rocky', lurePoints: {'Jig': 0.2, 'Crankbait': 0.4, 'Jerkbait': 0.2, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': -0.4, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0.1}},
-        {answer: 'Soft', lurePoints: {'Jig': -0.3, 'Crankbait': 0.1, 'Jerkbait': 0.1, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': 0.1, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0.0}},
-        {answer: 'Grassy', lurePoints: {'Jig': 0.1, 'Crankbait': -0.05, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': -.05, 'Wacky rig': 0.1, 'Drop shot': 0, 'Glidebait': 0.0, 'Topwater': 0, 'Chatterbait': 0.1}},
-        {answer: 'Not sure', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0}}
-      ]
-    },
-    {
-      text: 'How deep is the area that you are fishing?',
-      options: [
-        {answer: '< 1 foot', lurePoints: {'Jig': -0.1, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': -0.1, 'Wacky rig': 0.05, 'Drop shot': -0.15, 'Glidebait': -0.25, 'Topwater': 0.2, 'Chatterbait': 0.05}},
-        {answer: '1 to 5 feet', lurePoints: {'Jig': 0.05, 'Crankbait': 0.1, 'Jerkbait': 0.1, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0.1, 'Drop shot': -0.05, 'Glidebait': -0.05, 'Topwater': 0, 'Chatterbait': 0}},
-        {answer: '5 to 15 feet', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0.1, 'Swimbait': 0, 'Spinnerbait': 0.05, 'Wacky rig': 0.15, 'Drop shot': 0.1, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0}},
-        {answer: '15+ feet', lurePoints: {'Jig': 0.1, 'Crankbait': 0, 'Jerkbait': -0.15, 'Swimbait': 0, 'Spinnerbait': 0.05, 'Wacky rig': 0.2, 'Drop shot': 0.1, 'Glidebait': 0, 'Topwater': -0.15, 'Chatterbait': 0}},
-      ]
-    },
-    {
-      text: 'Is it windy? (10mph+)',
-      options: [
-        {answer: 'yes', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0.1, 'Spinnerbait': 0.1, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0.4, 'Topwater': 0, 'Chatterbait': 0}},
-        {answer: 'no',lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0.05, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': -0.2, 'Topwater': 0.1, 'Chatterbait': 0}}
-      ]
-    },
-    {
-      text: 'How is the water temperature?',
-      options: [
-        {answer: 'About normal', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0}},
-        {answer: 'Warmer than usual', lurePoints: {'Jig': 0, 'Crankbait': 0.1, 'Jerkbait': 0.2, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0.15, 'Topwater': 0.25, 'Chatterbait': 0.15}},
-        {answer: 'Colder than usual', lurePoints: {'Jig': 0.2, 'Crankbait': -0.2, 'Jerkbait': -0.2, 'Swimbait': -0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': 0.2, 'Glidebait': -0.15, 'Topwater': -0.3, 'Chatterbait': 0.05}}
-      ]
-    }
-  ]
-
 
 
   {if (quizCompleted) {
@@ -109,8 +41,6 @@ function App() {
       return (<QuizCard text={opt.answer} clickAction={() => handleCardClick(opt)}/>)
     })}
     </CardGroup>
-  
-    {/* <QuizSlide question={questions[currentQuestion].text} cards={questions[currentQuestion].options}/> */}
     </>
   );
 }
--- fishing-mentor/src/helpers.js
+++ fishing-mentor/src/helpers.js
@@ -10,4 +10,68 @@ const updateResults = (newData, existing) => {
     return newResults
 }
 
-export default updateResults
+const questions = [
+    {
+      text: 'Are you fishing from shore?',
+      options: [
+        {answer: 'Yes', lurePoints: {'Jig': 0.1, 'Crankbait': 0.1, 'Jerkbait': 0.15, 'Swimbait': 0, 'Spinnerbait': 0.05, 'Wacky rig': 0.1, 'Drop shot': -0.4, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0.05}},
+        {answer: 'No', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': 0.4, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0.05}}
+        
+      ]
+    },
+    {
+      text: 'Are you getting caught up in vegetation / debris?',
+      options: [
+        {answer: 'Yes', lurePoints: {'Jig': 0.2, 'Crankbait': -0.3, 'Jerkbait': 0, 'Swimbait': 0.1, 'Spinnerbait': -0.1, 'Wacky rig': 0.3, 'Drop shot': 0, 'Glidebait': -0.4, 'Topwater': 0.2, 'Chatterbait': 0.1}},
+        {answer: 'No', lurePoints: {'Jig': 0, 'Crankbait': 0.1, 'Jerkbait': 0.1, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0.0}}
+      ]
+    },
+    {
+      text: 'How is the bottom of the fishery?',
+      options: [
+        {answer: 'Rocky', lurePoints: {'Jig': 0.2, 'Crankbait': 0.4, 'Jerkbait': 0.2, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': -0.4, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0.1}},
+        {answer: 'Soft', lurePoints: {'Jig': -0.3, 'Crankbait': 0.1, 'Jerkbait': 0.1, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': 0.1, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0.0}},
+        {answer: 'Grassy', lurePoints: {'Jig': 0.1, 'Crankbait': -0.05, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': -.05, 'Wacky rig': 0.1, 'Drop shot': 0, 'Glidebait': 0.0, 'Topwater': 0, 'Chatterbait': 0.1}},
+        {answer: 'Not sure', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0}}
+      ]
+    },
+    {
+      text: 'How deep is the area that you are fishing?',
+      options: [
+        {answer: '< 1 foot', lurePoints: {'Jig': -0.1, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': -0.1, 'Wacky rig': 0.05, 'Drop shot': -0.15, 'Glidebait': -0.25, 'Topwater': 0.2, 'Chatterbait': 0.05}},
+        {answer: '1 to 5 feet', lurePoints: {'Jig': 0.05, 'Crankbait': 0.1, 'Jerkbait': 0.1, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0.1, 'Drop shot': -0.05, 'Glidebait': -0.05, 'Topwater': 0, 'Chatterbait': 0}},
+        {answer: '5 to 15 feet', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0.1, 'Swimbait': 0, 'Spinnerbait': 0.05, 'Wacky rig': 0.15, 'Drop shot': 0.1, 'Glidebait': 0.1, 'Topwater': 0, 'Chatterbait': 0}},
+        {answer: '15+ feet', lurePoints: {'Jig': 0.1, 'Crankbait': 0, 'Jerkbait': -0.15, 'Swimbait': 0, 'Spinnerbait': 0.05, 'Wacky rig': 0.2, 'Drop shot': 0.1, 'Glidebait': 0, 'Topwater': -0.15, 'Chatterbait': 0}},
+      ]
+    },
+    {
+      text: 'Is it windy? (10mph+)',
+      options: [
+        {answer: 'yes', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0.1, 'Spinnerbait': 0.1, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0.4, 'Topwater': 0, 'Chatterbait': 0}},
+        {answer: 'no',lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0.05, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': -0.2, 'Topwater': 0.1, 'Chatterbait': 0}}
+      ]
+    },
+    {
+      text: 'How is the water temperature?',
+      options: [
+        {answer: 'About normal', lurePoints: {'Jig': 0, 'Crankbait': 0, 'Jerkbait': 0, 'Swimbait': 0, 'Spinnerbait': 0, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0, 'Topwater': 0, 'Chatterbait': 0}},
+        {answer: 'Warmer than usual', lurePoints: {'Jig': 0, 'Crankbait': 0.1, 'Jerkbait': 0.2, 'Swimbait': 0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0, 'Drop shot': 0, 'Glidebait': 0.15, 'Topwater': 0.25, 'Chatterbait': 0.15}},
+        {answer: 'Colder than usual', lurePoints: {'Jig': 0.2, 'Crankbait': -0.2, 'Jerkbait': -0.2, 'Swimbait': -0.1, 'Spinnerbait': 0.15, 'Wacky rig': 0.1, 'Drop shot': 0.2, 'Glidebait': -0.15, 'Topwater': -0.3, 'Chatterbait': 0.05}}
+      ]
+    }
+  ]
+
+  const lures = {
+    'Jig': 0,
+    'Crankbait': 0,
+    'Jerkbait': 0,
+    'Swimbait': 0,
+    'Spinnerbait': 0,
+    'Wacky rig': 0,
+    'Drop shot': 0,
+    'Glidebait': 0,
+    'Topwater': 0,
+    'Chatterbait': 0
+  }
+
+export { updateResults, questions, lures }
