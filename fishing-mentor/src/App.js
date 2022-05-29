af0a4d0d1b6427e0a32df29195244023aa35c7b5
unknown <nraisane@asu.edu>
2022-05-29 16:50:27 -0700

    tinkering to get animations to work ...

--- fishing-mentor/src/App.js
+++ fishing-mentor/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import TopNav from './components/TopNav'
 import './App.css';
@@ -6,40 +6,60 @@ import { CardGroup } from 'react-bootstrap';
 import QuizCard from './components/QuizCard'
 import Results from './components/Results'
 import { updateResults, questions, lures }  from './helpers.js'
+import placeholder_img from './img/blue_placeholder.png'
+import ReactCSSTransitionGroup from 'react-transition-group'
+import { useTransition, animated } from 'react-spring'
 
 function App() {
+  const [showCards, setShowCards] = useState(true)
+  const [wobble, setWobble] = useState(0)
   const [currentQuestion, setCurrentQuestion] = useState(0)
+  // const [currentCards, setCurrentCards] = useState(questions[currentQuestion].options)
   const [quizCompleted, setQuizCompleted] = useState(false)
   const [lureStatus, setLureStatus] = useState(lures)
 
   const handleCardClick = (selectedCard) => {
-    // If no more questions, the quiz is over
     if (currentQuestion >= questions.length - 1) {
       setQuizCompleted(true)
     }
+    setShowCards(true, () => {
+
+    })
+    setWobble(1)
     // Update lureStatus with appropriate values
     const r = updateResults(selectedCard.lurePoints, lureStatus)
     setLureStatus(r)
     // Increment to move on to next question
     const nextQuestion = currentQuestion + 1
     setCurrentQuestion(nextQuestion)
+    // If no more questions, the quiz is over
+    
   }
-
+  function generate_cards() {
+    console.log('executing generate_cards')
+    var cards = []
+    questions[currentQuestion].options.map((opt) => {
+      console.log('iteration')
+      cards.push(<QuizCard text={opt.answer} clickAction={() => handleCardClick(opt)} wobbleState={wobble}/>)
+    })
+    return cards
+  }
 
   {if (quizCompleted) {
     return (<Results result={JSON.stringify(lureStatus)}/>)
   } else {
   return (
     <>
+    {/* {console.log(currentCards)} */}
     <TopNav />
-    
+    {/* <img className='answerCards' src={placeholder_img}></img> */}
     <h1 className='questionHeader'>{questions[currentQuestion].text}</h1>
     <p>Question number {currentQuestion}</p>
 
-    <CardGroup className='cardGroup'>
-    {questions[currentQuestion].options.map((opt) => {
-      return (<QuizCard text={opt.answer} clickAction={() => handleCardClick(opt)}/>)
-    })}
+    <CardGroup className='cardGroup' wobble={wobble}>
+    {showCards ? generate_cards().map((card) => {
+      return card
+    }) : ''}
     </CardGroup>
     </>
   );
--- fishing-mentor/src/components/QuizCard.jsx
+++ fishing-mentor/src/components/QuizCard.jsx
@@ -1,11 +1,32 @@
 import Card from 'react-bootstrap/Card';
 import placeholder_img from '../img/blue_placeholder.png'
 import './QuizCard.css'
+import React, { useEffect } from 'react'
+import { useTransition, animated } from 'react-spring'
 
 const QuizCard = (props) => {
+    useEffect(() => {
+        console.log('mounting card useEffect')
+        return () => {
+            console.log('unmounting card useEffect')
+        }
+    }, [props.wobbleState])
+
+    // const transition = useTransition(props.wobbleState, {
+    //     from: { x: -400 },
+    //     enter: { x: 0},
+    //     leave: { x: 400 },
+    //   })
+
+    // transition((style, item) => 
+    //   item ? <animated.div style={style}>random</animated.div> : '' 
+    // )
     return (
         <>
         <Card onClick={props.clickAction} className='quiz-card'>
+            {/* {transition((style, item) => 
+                item ? <animated.div style={style}>random</animated.div> : '' 
+            )} */}
             <Card.Img variant="top" src={placeholder_img} />
             <Card.Body>
             <Card.Text className="quiz-card-text">
